e89e4d4f344e3688313a6740849143fb3d6c856b
Inception <skycloud11102@126.com>
2023-08-09 20:45:03 +0800

    mmap first commit

--- user/user.h
+++ user/user.h
@@ -23,6 +23,8 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
+void *mmap(void *, unsigned int, int, int, int, unsigned int);
+int munmap(void *, unsigned int);
 
 // ulib.c
 int stat(const char*, struct stat*);
